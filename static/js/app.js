0f810f7f28ba127da2d4e7dfbd314dc38f9dde32
v4l3-v1ll4rr03l <villarroelvalentina@MacBook-Air-6.attlocal.net>
2023-10-04 18:56:15 -0400

    still working

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,31 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"; 
-
-d3.json(url).then(function(data) { console.log(data); });
-
 let otu_labels = [];
 let otu_amounts = [];
-let samples = data.samples;
 
-for (let i = 0; i < samples.length; i++)
-{
-    for (let i = 0; j < samples.sample_values.length; j++)
+d3.json(url).then(function(data) { 
+    console.log(data); 
+    let samples = data.samples;
+
+    for (let i = 0; i < samples.length; i++)
     {
-        let curr_id = samples.otu_ids[i];
-        let curr_amt = samples.sample_values[i];
-        let index = otu_labels.indexOf(curr_id);
-        if (index < 0)
+        for (let j = 0; j < samples[0].sample_values.length; j++)
         {
-            otu_labels.push(curr_id);
-            otu_amounts.push(curr_amt);
-        }
-        else
-        {
-            otu_amounts[index] = otu_amounts[index] + curr_amt;
+            let curr_id = samples[i].otu_ids[j];
+            let curr_amt = samples[i].sample_values[j];
+            let index = otu_labels.indexOf(curr_id);
+            if (index < 0)
+            {
+                otu_labels.push(curr_id);
+                otu_amounts.push(curr_amt);
+            }
+            else
+            {
+                otu_amounts[index] = otu_amounts[index] + curr_amt;
+            }
         }
     }
-}
+
+});
 
 let trace1 = {
     x: otu_amounts,
@@ -32,4 +34,9 @@ let trace1 = {
     type: "bar",
     orientation: "h"
   };
+  
+  let data = [trace1];
+  
+  // Render the plot to the div tag with id "plot"
+  Plotly.newPlot("plot", data);
   
