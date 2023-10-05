69b651f4c57d79699ecb1dfebbda3446f08f4321
v4l3-v1ll4rr03l <villarroelvalentina@MacBook-Air-6.attlocal.net>
2023-10-05 01:05:14 -0400

    data clean

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,49 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"; 
-let otu_labels = [];
-let otu_amounts = [];
+let curr_id = 940;
+let curr_data =[];
+let curr_metadata = {};
 
 d3.json(url).then(function(data) { 
     console.log(data); 
-    let samples = data.samples;
-
-    for (let i = 0; i < samples.length; i++)
+    for (let i = 0; i < data.samples.length; i++)
     {
-        for (let j = 0; j < samples[0].sample_values.length; j++)
+        let temp_samples = data.samples[i];
+        if (temp_samples.id == curr_id)
         {
-            let curr_id = samples[i].otu_ids[j];
-            let curr_amt = samples[i].sample_values[j];
-            let index = otu_labels.indexOf(curr_id);
-            if (index < 0)
+            curr_metadata = data.metadata[i];
+            for (let j = 0; j < temp_samples.sample_values.length; j++)
             {
-                otu_labels.push(curr_id);
-                otu_amounts.push(curr_amt);
-            }
-            else
-            {
-                otu_amounts[index] = otu_amounts[index] + curr_amt;
+                curr_data.push({"otu_id": temp_samples.otu_ids[j], 
+                                "otu_label": temp_samples.otu_labels[j], 
+                                "sample_value": temp_samples.sample_values[j]});
             }
         }
     }
+    let sorted_data = curr_data.sort((a, b) => b.sample_value - a.sample_value);
+    let sliced_data = sorted_data.slice(0,10);
+    console.log(sliced_data);
+    console.log(curr_metadata);
 
+    let trace1 = {
+        x: sliced_data.map(object => object.sample_value),
+        y: sliced_data.map(object => object.out_id),
+        type: "bar",
+        orientation: "h"
+      };
+      
+    Plotly.newPlot("plot", [trace1]);
 });
 
-let trace1 = {
-    x: otu_amounts,
-    y: otu_labels,
-    text: otu_labels,
-    type: "bar",
-    orientation: "h"
-  };
+// let trace1 = {
+//     x: otu_amounts,
+//     y: otu_labels,
+//     text: otu_labels,
+//     type: "bar",
+//     orientation: "h"
+//   };
   
-  let data = [trace1];
+//   let data = [trace1];
   
-  // Render the plot to the div tag with id "plot"
-  Plotly.newPlot("plot", data);
+//   // Render the plot to the div tag with id "plot"
+//   Plotly.newPlot("plot", data);
   
