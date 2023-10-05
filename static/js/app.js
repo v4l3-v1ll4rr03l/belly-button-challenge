b77bfc07c56bd23ed2dde128d376c0e3fc05c5d7
v4l3-v1ll4rr03l <villarroelvalentina@MacBook-Air-6.attlocal.net>
2023-10-05 11:44:54 -0400

    bar chart done

--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,30 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"; 
-let curr_id = 940;
-let curr_data =[];
-let curr_metadata = {};
+buildChart(940);
 
-d3.json(url).then(function(data) { 
-    console.log(data); 
-    for (let i = 0; i < data.samples.length; i++)
-    {
-        let temp_samples = data.samples[i];
-        if (temp_samples.id == curr_id)
-        {
-            curr_metadata = data.metadata[i];
-            for (let j = 0; j < temp_samples.sample_values.length; j++)
-            {
-                curr_data.push({"otu_id": temp_samples.otu_ids[j], 
-                                "otu_label": temp_samples.otu_labels[j], 
-                                "sample_value": temp_samples.sample_values[j]});
-            }
-        }
-    }
-    let sorted_data = curr_data.sort((a, b) => b.sample_value - a.sample_value);
-    let sliced_data = sorted_data.slice(0,10);
-    console.log(sliced_data);
-    console.log(curr_metadata);
+function labelID(otu_id) { return "OTU".concat(" ", otu_id.toString()); }
 
-    let trace1 = {
-        x: sliced_data.map(object => object.sample_value),
-        y: sliced_data.map(object => object.out_id),
-        type: "bar",
-        orientation: "h"
-      };
-      
-    Plotly.newPlot("plot", [trace1]);
-});
+function buildChart(curr_id){
+    d3.json(url).then(function(data) { 
+        console.log(data);
+        samples = data.samples;
+        console.log(samples);  
+        let curr_sample = samples.filter(sample => sample.id == curr_id);
+        curr_sample = curr_sample[0];
+        console.log(curr_sample);
+        let temp_x = curr_sample.sample_values;
+        let temp_y = curr_sample.otu_ids;
+        console.log(temp_x);
+        console.log(temp_y);
+        
+        let trace1 = {
+            x: temp_x.slice(0,10).reverse(),
+            y: temp_y.slice(0,10).reverse().map(labelID),
+            type: "bar",
+            orientation: "h"
+          };
+          
+        Plotly.newPlot("plot", [trace1]);
+    });
+}
+
 console.log(curr_metadata);
